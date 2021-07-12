3aa74620d20f67c73ad91ce5a474822cd848d80f
jonkarrer <karrerjon@gmail.com>
2021-07-12 11:44:02 -0500

    adding fip cards

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 import { toggleDarkMode } from "./modules/toggleDarkMode.js";
 
-document.getElementById('dark_mode_switch')
-.addEventListener('click', () => toggleDarkMode());
+document
+  .getElementById("dark_mode_switch")
+  .addEventListener("click", () => toggleDarkMode());
+
+document.querySelectorAll(".flipButton").forEach((item) => {
+  item.addEventListener("click", () => {
+    const card = document.querySelector(".card");
+    card.classList.toggle("is-flipped");
+  });
+});
